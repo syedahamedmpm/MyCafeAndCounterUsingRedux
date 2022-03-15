ff4476766fa800f84b4e3d25ba899555b3255a4c
syedahamedmpm <salearning01@gmail.com>
2022-03-15 18:25:17 +0530

    Successfully Done

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
+import { incCount,decCount } from "../actions/counterActions";
 
 class Counter extends React.Component{
+    constructor(props){
+        super(props)
+        this.incCount = this.incCount.bind(this);
+        this.decCount = this.decCount.bind(this);
+    }
     incCount = (e) => {
+        console.log("incCount")
         e.preventDefault()
         this.props.incCount(this.state)
     }
     decCount = (e) => {
+        console.log("decCount")
         e.preventDefault()
         this.props.decCount(this.state)
     }
@@ -15,7 +23,7 @@ class Counter extends React.Component{
         return(
             <div>
                 
-                <p>The Count is {this.props.counters}</p>
+                <h1>The Count is {this.props.counters}</h1>
                 <button className="btn btn-primary" onClick={this.incCount} >+</button>
                 <button className="btn btn-primary" onClick={this.decCount}>-</button>
             </div>
@@ -23,7 +31,6 @@ class Counter extends React.Component{
     }
 }
 Counter.propTypes = {
-    count : PropTypes.number.isRequired,
     incCount: PropTypes.func.isRequired,
     decCount: PropTypes.func.isRequired
 }
@@ -32,5 +39,5 @@ const mapStateToProps = (state) =>({
 })
 export default connect(
     mapStateToProps,
-    null
+    { incCount,decCount }
 ) (Counter);
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,6 +1,6 @@
 import { INCREMENT_COUNT, DECREMENT_COUNT } from '../actions/types';
 
-const initialState = [];
+const initialState = 0;
 
 const counterReducer = (state = initialState, action) => {
   switch (action.type) {
